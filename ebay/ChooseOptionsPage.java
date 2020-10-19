1fc7ff325afc2a121bd496d0e20749adfd203da9
Marta <61636596+martao7@users.noreply.github.com>
2020-10-19 21:01:00 +0200

    Add files via upload

---
+++ ebay/ChooseOptionsPage.java
@@ -0,0 +1,32 @@
+package ebay;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ChooseOptionsPage {
+
+    public WebDriver driver;
+
+    public ChooseOptionsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(className = "fake-tabs__item btn")
+    private WebElement buyItNowOption;
+
+
+    @FindBy(className = "checkbox__control")
+    private WebElement conditionNewCheck;
+
+
+    public void setOptionToChoose() {
+        buyItNowOption.click();
+
+        if (!conditionNewCheck.isSelected())
+            conditionNewCheck.click();
+    }
+}
+
